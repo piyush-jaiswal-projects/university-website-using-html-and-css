3c671767617812018cfa0414c29c72199a62ed5a
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-07-29 17:49:19 +0530

    Form Validation Complete

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 function formValidation(){
-    var form = document.getElementById("query-form");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
 
     let name = document.getElementById("userName").value;
     let email = document.getElementById("userEmail").value;
     let phone = document.getElementById("userPhone").value;
     let query = document.getElementById("userQuery").value;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     console.log("Query Details:" + name + email + phone + query);
     if(name.length === 0){
         alert("Name field can't be empty");
     }
-    else if(email.length === 0){
-        alert("Email field can't be empty");
-    }
-    // else if(){
-    //     // check email
-    // }
+    else if(email.length === 0 || !email.match(validRegex)){
+            alert("Invalid email address");
+        }
     else if(phone.length != 10){
         alert("Phone number should be of 10 digits");
     }
